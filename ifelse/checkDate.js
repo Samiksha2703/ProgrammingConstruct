9452424b4f316b13806a96bbc1bbf738e1eaf5c1
samiksha Shende <shende.samiksha@rediffmail.com>
2020-12-02 14:31:29 +0530

    Add ifelse program files

---
+++ ifelse/checkDate.js
@@ -0,0 +1,9 @@
+const START_DATE = 2292020;
+const END_DATE = 7012020;
+//let date = parseInt(prompt("Enter date in the format mddYYYY"));
+let date = 7212020;
+if (START_DATE < date && END_DATE > date)
+        console.log("Entered date is in the range");
+else
+    console.log("Entered date is not in the range");
+
---
+++ ifelse/findMinMax.js
@@ -0,0 +1,21 @@
+const count = 5;
+let maxValue = 0;
+let minValue = 0;
+let index = 1
+while(index <= count){
+    let number = Math.floor(100 + Math.random() * 900 );
+if (index == 1 ){
+    maxValue = number;
+    minValue = number;
+}
+if (number < minValue ){
+    minValue = number;
+}
+else if (number > maxValue){
+    maxValue = number;
+}
+index++;
+}
+console.log("Maximum Value : "+maxValue+" Minimum Value : "+minValue);
+       
+	
